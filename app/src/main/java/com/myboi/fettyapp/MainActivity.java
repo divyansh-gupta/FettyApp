61b6c26499c3c073d34c7dcd29c1a6ad6da207af
divyansh-gupta <divyg@vt.edu>
2016-03-27 23:09:21 -0400

    Added basic usage button

--- app/src/main/java/com/myboi/fettyapp/MainActivity.java
+++ app/src/main/java/com/myboi/fettyapp/MainActivity.java
@@ -12,9 +12,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    MusicPlayer player = new MusicPlayer(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +43,15 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Button fettyButton = (Button) findViewById(R.id.fettyButton);
+        fettyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FettyNoise fettyNoise = new FettyNoise("aye.mp3");
+                player.findAndPlaySong(fettyNoise);
+            }
+        });
     }
 
     @Override
