3dbcc84a467604ae8da6d316a6765386c6afe4a7
divyansh-gupta <divyg@vt.edu>
2016-03-27 22:21:49 -0400

    Added Fettys Noise

---
+++ app/src/main/java/com/myboi/fettyapp/FettyNoise.java
@@ -0,0 +1,14 @@
+package com.myboi.fettyapp;
+
+import android.content.Context;
+
+/**
+ * Created by divy9677 on 3/27/16.
+ */
+public class FettyNoise implements Playable {
+
+    public String getRawResourcePath(Context context) {
+        return null;
+    }
+
+}
